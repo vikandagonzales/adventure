4d8ea544ac03551544bd873a2b42967b27ee1a66
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-12-26 23:26:07 -0800

    added new colors for variables

--- src/components/Main.js
+++ src/components/Main.js
@@ -41,6 +41,7 @@ class Main extends React.Component {
       <div id="main">
         <div className="container has-text-centered">
           <h1 className="title is-1">Welcome Baby</h1>
+          <p className="subtitle is-3">Keeghan & Vika are having a baby!</p>
           <span className="button" onClick={this.invite}>Invitation</span>
         </div>       
         <Login modalClasses={this.state.modalClasses} toggle={this.toggle} history={this.props.history} />
