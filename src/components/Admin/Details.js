ffa5a1a0a8976ecc9b9b25438f30ca911e972698
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2019-01-01 23:22:45 -0800

    added styling to details tab in admin modal

--- src/components/Admin/Details.js
+++ src/components/Admin/Details.js
@@ -14,44 +14,53 @@ class Details extends React.Component {
     const rightColumn = 'column is-9';
     return (
       <div>
-        <div className="columns">
-          <div className={leftColumn}><i className="fa fas fa-male"></i>Father</div>
-          <div className={rightColumn}>{details.father}</div>
+        <div className="buttons">
+          <span className="button is-primary">
+            <span className="icon">
+              <i className="fa fas fa-pen"></i>
+            </span>
+          </span>
         </div>
-        <div className="columns">
-          <div className={leftColumn}><i className="fa fas fa-female"></i>Mother</div>
-          <div className={rightColumn}>{details.mother}</div>
-        </div>
-        <div className="columns">
-          <div className={leftColumn}><i className="fa fas fa-concierge-bell"></i>Host</div>
-          <div className={rightColumn}>{details.host}</div>
-        </div>
-        <div className="columns">
-          <div className={leftColumn}><i className="fa fas fa-phone"></i>Contact</div>
-          <div className={rightColumn}>{details.host_contact}</div>
-        </div>
-        <div className="columns">
-          <div className={leftColumn}><i className="fa fas fa-envelope"></i>Deadline</div>
-          <div className={rightColumn}>{details.rsvp_date}</div>
-        </div>
-        <div className="columns">
-          <div className={leftColumn}><i className="fa fas fa-calendar"></i>Date</div>
-          <div className={rightColumn}>{details.date}</div>
-        </div>
-        <div className="columns">
-          <div className={leftColumn}><i className="fa fas fa-clock"></i>Time</div>
-          <div className={rightColumn}>{details.time}</div>
-        </div>
-        <div className="columns">
-          <div className={leftColumn}><i className="fa fas fa-map-marker-alt"></i>Location</div>
-          <div className={rightColumn}>{details.location}</div>
-        </div>
-        <div className="columns">
-          <div className={leftColumn}><i className="fa fas fa-map"></i>Map</div>
-          <div className={rightColumn}>{details.map}</div>
-        </div>       
-        <div>
-          <iframe title="location" src={details.map}></iframe>
+        <div className="details">
+          <div className="columns">
+            <div className={leftColumn}><i className="fa fas fa-male"></i>Father</div>
+            <div className={rightColumn}>{details.father}</div>
+          </div>
+          <div className="columns">
+            <div className={leftColumn}><i className="fa fas fa-female"></i>Mother</div>
+            <div className={rightColumn}>{details.mother}</div>
+          </div>
+          <div className="columns">
+            <div className={leftColumn}><i className="fa fas fa-concierge-bell"></i>Host</div>
+            <div className={rightColumn}>{details.host}</div>
+          </div>
+          <div className="columns">
+            <div className={leftColumn}><i className="fa fas fa-phone"></i>Contact</div>
+            <div className={rightColumn}>{details.host_contact}</div>
+          </div>
+          <div className="columns">
+            <div className={leftColumn}><i className="fa fas fa-envelope"></i>Deadline</div>
+            <div className={rightColumn}>{details.rsvp_date}</div>
+          </div>
+          <div className="columns">
+            <div className={leftColumn}><i className="fa fas fa-calendar"></i>Date</div>
+            <div className={rightColumn}>{details.date}</div>
+          </div>
+          <div className="columns">
+            <div className={leftColumn}><i className="fa fas fa-clock"></i>Time</div>
+            <div className={rightColumn}>{details.time}</div>
+          </div>
+          <div className="columns">
+            <div className={leftColumn}><i className="fa fas fa-map-marker-alt"></i>Location</div>
+            <div className={rightColumn}>{details.location}</div>
+          </div>
+          <div className="columns">
+            <div className={leftColumn}><i className="fa fas fa-map"></i>Map</div>
+            <div className={rightColumn}>{details.map}</div>
+          </div>
+          <div>
+            <iframe title="location" src={details.map}></iframe>
+          </div>
         </div>
       </div>
     );
