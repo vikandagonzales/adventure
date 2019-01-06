7e2895dd1ea2269d6b05d350ab663bfa40671a48
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2019-01-06 02:35:08 -0800

    added functionality to edit registry in admin

--- src/components/Admin/Registries.js
+++ src/components/Admin/Registries.js
@@ -4,7 +4,14 @@ import React from 'react';
 // REDUX
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
-import {addRegistry, addRegistryReset, deleteRegistry, deleteRegistryReset} from '../../state/actions/registries';
+import {
+  addRegistry,
+  addRegistryReset,
+  editRegistry,
+  editRegistryReset,
+  deleteRegistry,
+  deleteRegistryReset
+} from '../../state/actions/registries';
 
 // COMPONENTS
 import Registry from './Registry';
@@ -45,12 +52,22 @@ class Registries extends React.Component {
               addRegistryError={this.props.addRegistryError}
             />
           ) : null
-        }  
+        }
+        {
+          this.props.editRegistryError ? (
+            <p className="help is-danger has-text-centered">
+              Could not edit registry.
+                </p>
+          ) : null
+        }
         {
           registries.map((registry, i) => {
             return (
               <Registry
                 key={i}
+                editRegistry={this.props.editRegistry}
+                editRegistryReset={this.props.editRegistryReset}
+                editegistryError={this.props.editRegistryError}
                 deleteRegistry={this.props.deleteRegistry}
                 deleteRegistryReset={this.props.deleteRegistryReset}
                 deleteRegistryError={this.props.deleteRegistryError}
@@ -66,12 +83,15 @@ class Registries extends React.Component {
 
 const mapStateToProps = state => ({
   addRegistryError: state.registries.addRegistryError,
+  editRegistryError: state.registries.editRegistryError,
   deleteRegistryError: state.registries.deleteRegistryError
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators({
   addRegistry,
   addRegistryReset,
+  editRegistry,
+  editRegistryReset,
   deleteRegistry,
   deleteRegistryReset
 }, dispatch);
--- src/components/Admin/Registry.js
+++ src/components/Admin/Registry.js
@@ -10,9 +10,18 @@ class Registry extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
+      edit: false,
       modal: false,
-      modalClasses: 'modal'
+      modalClasses: 'modal',
+      store: this.props.registry.store,
+      logo: this.props.registry.logo,
+      url: this.props.registry.url
     };
+  };
+
+  edit = () => {
+    this.props.editRegistryReset();
+    this.setState({edit: !this.state.edit});
   };
 
   toggle = () => {
@@ -30,51 +39,129 @@ class Registry extends React.Component {
     }
   };
 
+  editRegistry = async event => {
+    event.preventDefault();
+    const {store, url, logo} = this.state;
+    const registry = {store, url, logo};
+    await this.props.editRegistry(registry, this.props.registry.id);
+    if (!this.props.editRegistryError) this.edit();
+  };
+
   render () {
     const registry = this.props.registry;
     const leftColumn = "column is-3";
     const rightColumn = "column is-9";
     return (
       <div>
-        <div className="columns">
-          <div className={leftColumn}>
-            <figure className="image">
-              <img src={registry.logo} alt={registry.store} />
-            </figure>
+        <form className="edit-registry" onSubmit={this.editRegistry}>
+          <div className="columns">
+            <div className={leftColumn}>
+              <figure className="image">
+                <img src={registry.logo} alt={registry.store} />
+              </figure>
+            </div>
+            <div className={rightColumn}>
+              <ul>
+                <li>
+                  <i className="fa fas fa-store"></i>
+                  {
+                    this.state.edit ? (
+                      <div className="field">
+                        <p className="control">
+                          <input
+                            className="input"
+                            type="text"
+                            placeholder="Store"
+                            value={this.state.store}
+                            onChange={event => this.setState({store: event.target.value})}
+                            required
+                          />
+                        </p>
+                      </div>
+                    ) : (
+                      <span>{registry.store}</span>
+                    )
+                  }
+                </li>
+                <li>
+                  <i className="fa fas fa-image"></i>
+                  {
+                    this.state.edit ? (
+                      <div className="field">
+                        <p className="control">
+                          <input
+                            className="input"
+                            type="text"
+                            placeholder="Logo"
+                            value={this.state.logo}
+                            onChange={event => this.setState({logo: event.target.value})}
+                            required
+                          />
+                        </p>
+                      </div>
+                    ) : (
+                      <a href={registry.logo} target="_blank" rel="noopener noreferrer">
+                        {registry.logo.length > 30 ? registry.logo.substring(0, 30) + '...' : registry.logo}
+                      </a>
+                    )
+                  }
+                </li>
+                <li>
+                  <i className="fa fas fa-link"></i>
+                  {
+                    this.state.edit ? (
+                      <div className="field">
+                        <p className="control">
+                          <input
+                            className="input"
+                            type="text"
+                            placeholder="URL"
+                            value={this.state.url}
+                            onChange={event => this.setState({url: event.target.value})}
+                            required
+                          />
+                        </p>
+                      </div>
+                    ) : (
+                      <a href={registry.url} target="_blank" rel="noopener noreferrer">
+                        {registry.url.length > 30 ? registry.url.substring(0, 30) + '...' : registry.url}
+                      </a>
+                    )
+                  }                 
+                </li>
+              </ul>
+            </div>
           </div>
-          <div className={rightColumn}>
-            <ul>
-              <li>
-                <i className="fa fas fa-store"></i>
-                {registry.store}
-              </li>
-              <li>
-                <i className="fa fas fa-image"></i>
-                <a href={registry.logo} target="_blank" rel="noopener noreferrer">
-                  {registry.logo.length > 30 ? registry.logo.substring(0, 30) + '...' : registry.logo}
-                </a>
-              </li>
-              <li>
-                <i className="fa fas fa-link"></i>
-                <a href={registry.url} target="_blank" rel="noopener noreferrer">
-                  {registry.url.length > 30 ? registry.url.substring(0, 30) + '...' : registry.url}
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="buttons is-right">
-          <span className="button is-small is-primary">
-            <span className="icon">
-              <i className="fa fas fa-pen"></i>
-            </span>
-          </span>
-          <span className="button is-small is-danger" onClick={this.toggle}>
-            <span className="icon">
-              <i className="fa fas fa-trash-alt"></i>
-            </span>
-          </span>
-        </div>
+          {
+            !this.state.edit ? (
+              <div className="buttons is-right">
+                <span className="button is-small is-primary" onClick={this.edit}>
+                  <span className="icon">
+                    <i className="fa fas fa-pen"></i>
+                  </span>
+                </span>
+                <span className="button is-small is-danger" onClick={this.toggle}>
+                  <span className="icon">
+                    <i className="fa fas fa-trash-alt"></i>
+                  </span>
+                </span>
+              </div>
+            ) : (
+              <div className="buttons is-right">
+                <span className="button is-small" onClick={this.edit}>
+                  <span className="icon">
+                    <i className="fa fas fa-times"></i>
+                  </span>
+                </span>
+                <button className="button is-small is-success">
+                  <span className="icon">
+                    <i className="fa fas fa-check"></i>
+                  </span>
+                </button>
+              </div>            
+            )
+          }
+        </form>
         <RegistryDelete
           modalClasses={this.state.modalClasses}
           toggle={this.toggle}
